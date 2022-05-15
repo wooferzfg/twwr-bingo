fd206857dfb27a5bda0394625b0634fa80058eb5
wooferzfg <spapushin@gmail.com>
2022-05-14 20:04:35 -0400

    Explain Bingosync support

--- tables/twwr.js
+++ tables/twwr.js
@@ -1,148 +1,154 @@
-var bingoList = [
-    [],
-    [
-        { "name": "Visit 3 Beedle Shops", "types": ["beedle"] },
-        { "name": "Talk to 2 Old Man Ho Hos", "types": ["hoho"] },
-        { "name": "Read 2 Letters", "types": ["letters"] },
-    ],
+var bingoList = (
 
+    // This JSON array can be copy and pasted into Bingosync
     [
-        { "name": "Fly a Seagull in 2 Sectors", "types": ["seagull"] },
-        { "name": "Collect an Item from 2 Minigames" },
-        { "name": "Talk to 4 Old Man Ho Hos", "types": ["hoho"] },
-    ],
+        [
+            { "name": "Visit 3 Beedle Shops", "types": ["beedle"] },
+            { "name": "Talk to 2 Old Man Ho Hos", "types": ["hoho"] },
+            { "name": "Read 2 Letters", "types": ["letters"] }
+        ],
 
-    [
-        { "name": "Visit 6 Beedle Shops", "types": ["beedle"] },
-        { "name": "Collect 5 Joy Pendants", "types": ["spoils"] },
-        { "name": "Feed 5 Fishmen", "types": ["fishmen"] },
-    ],
-    [
-        { "name": "Collect 5 Red Chu Jelly", "types": ["spoils"] },
-        { "name": "Collect 3 Small Keys and Big Keys", "types": ["keys", "dungeons"] },
-        { "name": "Collect 5 Green Chu Jelly", "types": ["spoils"] },
-    ],
-    [
-        { "name": "Collect 5 Skull Necklaces", "types": ["spoils"] },
-        { "name": "Visit 9 Beedle Shops", "types": ["beedle"] },
-        { "name": "Water 2 Korok Trees", "types": ["korok_trees"] },
-    ],
+        [
+            { "name": "Fly a Seagull in 2 Sectors", "types": ["seagull"] },
+            { "name": "Collect an Item from 2 Minigames" },
+            { "name": "Talk to 4 Old Man Ho Hos", "types": ["hoho"] }
+        ],
 
-    [
-        { "name": "Fly a Seagull in 4 Sectors", "types": ["seagull"] },
-        { "name": "Feed 10 Fishmen", "types": ["fishmen"] },
-        { "name": "Collect 4 Blue Chu Jelly", "types": ["blue_jelly"] },
-    ],
-    [
-        { "name": "Read 4 Letters", "types": ["letters"] },
-        { "name": "Collect Item from 2 Great Fairies", "types": ["fairies"] },
-        { "name": "Win 1 Item from the Auction" },
-    ],
-    [
-        { "name": "Open 6 Great Sea Chests", "types": ["great_sea"] },
-        { "name": "Collect 5 Golden Feathers", "types": ["spoils"] },
-        { "name": "Collect Item from 3 Great Fairies", "types": ["fairies"] },
-    ],
-    [
-        { "name": "Open 2 Chests in Submarines", "types": ["submarines"] },
-        { "name": "Fly a Seagull in 6 Sectors", "types": ["seagull"] },
-        { "name": "Talk to 6 Old Man Ho Hos", "types": ["hoho"] },
-    ],
-    [
-        { "name": "Collect Item from 4 Great Fairies", "types": ["fairies"] },
-        { "name": "Water 4 Korok Trees", "types": ["korok_trees"] },
-        { "name": "Collect 7 Blue Chu Jelly", "types": ["blue_jelly"] },
-    ],
-    [
-        { "name": "Feed 15 Fishmen", "types": ["fishmen"] },
-        { "name": "Read 6 Letters", "types": ["letters"] },
-        { "name": "Open 4 Chests in Secret Caves", "types": ["secret_caves"] },
-    ],
+        [
+            { "name": "Visit 6 Beedle Shops", "types": ["beedle"] },
+            { "name": "Collect 5 Joy Pendants", "types": ["spoils"] },
+            { "name": "Feed 5 Fishmen", "types": ["fishmen"] }
+        ],
+        [
+            { "name": "Collect 5 Red Chu Jelly", "types": ["spoils"] },
+            { "name": "Collect 3 Small Keys and Big Keys", "types": ["keys", "dungeons"] },
+            { "name": "Collect 5 Green Chu Jelly", "types": ["spoils"] }
+        ],
+        [
+            { "name": "Collect 5 Skull Necklaces", "types": ["spoils"] },
+            { "name": "Visit 9 Beedle Shops", "types": ["beedle"] },
+            { "name": "Water 2 Korok Trees", "types": ["korok_trees"] }
+        ],
 
-    [
-        { "name": "Enter 3 Warp Pots", "types": ["warp_pots", "dungeons"] },
-        { "name": "Water 6 Korok Trees", "types": ["korok_trees"] },
-        { "name": "Open 8 Dungeon Chests", "types": ["dungeon_chests", "dungeons"] },
-    ],
+        [
+            { "name": "Fly a Seagull in 4 Sectors", "types": ["seagull"] },
+            { "name": "Feed 10 Fishmen", "types": ["fishmen"] },
+            { "name": "Collect 4 Blue Chu Jelly", "types": ["blue_jelly"] }
+        ],
+        [
+            { "name": "Read 4 Letters", "types": ["letters"] },
+            { "name": "Collect Item from 2 Great Fairies", "types": ["fairies"] },
+            { "name": "Win 1 Item from the Auction" }
+        ],
+        [
+            { "name": "Open 6 Great Sea Chests", "types": ["great_sea"] },
+            { "name": "Collect 5 Golden Feathers", "types": ["spoils"] },
+            { "name": "Collect Item from 3 Great Fairies", "types": ["fairies"] }
+        ],
+        [
+            { "name": "Open 2 Chests in Submarines", "types": ["submarines"] },
+            { "name": "Fly a Seagull in 6 Sectors", "types": ["seagull"] },
+            { "name": "Talk to 6 Old Man Ho Hos", "types": ["hoho"] }
+        ],
+        [
+            { "name": "Collect Item from 4 Great Fairies", "types": ["fairies"] },
+            { "name": "Water 4 Korok Trees", "types": ["korok_trees"] },
+            { "name": "Collect 7 Blue Chu Jelly", "types": ["blue_jelly"] }
+        ],
+        [
+            { "name": "Feed 15 Fishmen", "types": ["fishmen"] },
+            { "name": "Read 6 Letters", "types": ["letters"] },
+            { "name": "Open 4 Chests in Secret Caves", "types": ["secret_caves"] }
+        ],
 
-    [
-        { "name": "Open 1 Miniboss Chest", "types": ["miniboss", "dungeons"] },
-        { "name": "Collect 2 Dungeon Maps and Compasses", "types": ["maps_compasses", "dungeons"] },
-        { "name": "Open 4 Chests on Lookout Platforms and Rafts", "types": ["lookout_platforms"] },
-    ],
+        [
+            { "name": "Enter 3 Warp Pots", "types": ["warp_pots", "dungeons"] },
+            { "name": "Water 6 Korok Trees", "types": ["korok_trees"] },
+            { "name": "Open 8 Dungeon Chests", "types": ["dungeon_chests", "dungeons"] }
+        ],
 
-    [
-        { "name": "Open 9 Great Sea Chests", "types": ["great_sea"] },
-        { "name": "Collect 10 Blue Chu Jelly", "types": ["blue_jelly"] },
-        { "name": "Place 3 Different Items on Windfall Pedestals", "types": ["pedestals"] },
-    ],
+        [
+            { "name": "Open 1 Miniboss Chest", "types": ["miniboss", "dungeons"] },
+            { "name": "Collect 2 Dungeon Maps and Compasses", "types": ["maps_compasses", "dungeons"] },
+            { "name": "Open 4 Chests on Lookout Platforms and Rafts", "types": ["lookout_platforms"] }
+        ],
 
-    [
-        { "name": "Defeat 1 Boss", "types": ["boss", "dungeons"] },
-        { "name": "Open 14 Dungeon Chests", "types": ["dungeon_chests", "dungeons"] },
-        { "name": "Open 4 Chests in Submarines", "types": ["submarines"] },
-    ],
+        [
+            { "name": "Open 9 Great Sea Chests", "types": ["great_sea"] },
+            { "name": "Collect 10 Blue Chu Jelly", "types": ["blue_jelly"] },
+            { "name": "Place 3 Different Items on Windfall Pedestals", "types": ["pedestals"] }
+        ],
 
-    [
-        { "name": "Defeat 2 Big Octos", "types": ["octos"] },
-        { "name": "Collect 5 Knight's Crests", "types": ["spoils"] },
-        { "name": "Enter 6 Warp Pots", "types": ["warp_pots", "dungeons"] },
-        { "name": "Use Command Melody in 2 Dungeons", "types": ["dungeons"] },
-    ],
+        [
+            { "name": "Defeat 1 Boss", "types": ["boss", "dungeons"] },
+            { "name": "Open 14 Dungeon Chests", "types": ["dungeon_chests", "dungeons"] },
+            { "name": "Open 4 Chests in Submarines", "types": ["submarines"] }
+        ],
 
-    [
-        { "name": "Collect 4 Dungeon Maps and Compasses", "types": ["maps_compasses", "dungeons"] },
-        { "name": "Open 6 Chests in Submarines", "types": ["submarines"] },
-        { "name": "Open 12 Great Sea Chests", "types": ["great_sea"] },
-    ],
+        [
+            { "name": "Defeat 2 Big Octos", "types": ["octos"] },
+            { "name": "Collect 5 Knight's Crests", "types": ["spoils"] },
+            { "name": "Enter 6 Warp Pots", "types": ["warp_pots", "dungeons"] },
+            { "name": "Use Command Melody in 2 Dungeons", "types": ["dungeons"] }
+        ],
 
-    [
-        { "name": "Open 8 Chests on Lookout Platforms and Rafts", "types": ["lookout_platforms"] },
-        { "name": "Defeat 4 Big Octos", "types": ["octos"] },
-        { "name": "Open 8 Chests in Secret Caves", "types": ["secret_caves"] },
-    ],
+        [
+            { "name": "Collect 4 Dungeon Maps and Compasses", "types": ["maps_compasses", "dungeons"] },
+            { "name": "Open 6 Chests in Submarines", "types": ["submarines"] },
+            { "name": "Open 12 Great Sea Chests", "types": ["great_sea"] }
+        ],
 
-    [
-        { "name": "Collect 6 Small Keys and Big Keys", "types": ["keys", "dungeons"] },
-        { "name": "Place 4 Different Items on Windfall Pedestals", "types": ["pedestals"] },
-        { "name": "Open 2 Miniboss Chests", "types": ["miniboss", "dungeons"] },
-    ],
+        [
+            { "name": "Open 8 Chests on Lookout Platforms and Rafts", "types": ["lookout_platforms"] },
+            { "name": "Defeat 4 Big Octos", "types": ["octos"] },
+            { "name": "Open 8 Chests in Secret Caves", "types": ["secret_caves"] }
+        ],
 
-    [
-        { "name": "Place 5 Different Items on Windfall Pedestals", "types": ["pedestals"] },
-        { "name": "Defeat 6 Big Octos", "types": ["octos"] },
-        { "name": "Break 2 Song Stones", "types": ["song_stones", "dungeons"] },
-    ],
+        [
+            { "name": "Collect 6 Small Keys and Big Keys", "types": ["keys", "dungeons"] },
+            { "name": "Place 4 Different Items on Windfall Pedestals", "types": ["pedestals"] },
+            { "name": "Open 2 Miniboss Chests", "types": ["miniboss", "dungeons"] }
+        ],
 
-    [
-        { "name": "Open 12 Chests on Lookout Platforms and Rafts", "types": ["lookout_platforms"] },
-        { "name": "Defeat 2 Bosses", "types": ["boss", "dungeons"] },
-        { "name": "Collect 9 Small Keys and Big Keys", "types": ["keys", "dungeons"] },
-    ],
+        [
+            { "name": "Place 5 Different Items on Windfall Pedestals", "types": ["pedestals"] },
+            { "name": "Defeat 6 Big Octos", "types": ["octos"] },
+            { "name": "Break 2 Song Stones", "types": ["song_stones", "dungeons"] }
+        ],
 
-    [
-        { "name": "Enter 9 Warp Pots", "types": ["warp_pots", "dungeons"] },
-        { "name": "Collect 6 Dungeon Maps and Compasses", "types": ["maps_compasses", "dungeons"] },
-        { "name": "Open 3 Miniboss Chests", "types": ["miniboss", "dungeons"] },
-    ],
+        [
+            { "name": "Open 12 Chests on Lookout Platforms and Rafts", "types": ["lookout_platforms"] },
+            { "name": "Defeat 2 Bosses", "types": ["boss", "dungeons"] },
+            { "name": "Collect 9 Small Keys and Big Keys", "types": ["keys", "dungeons"] }
+        ],
 
-    [
-        { "name": "Defeat 3 Bosses", "types": ["boss", "dungeons"] },
-        { "name": "Open 20 Dungeon Chests", "types": ["dungeon_chests", "dungeons"] },
-        { "name": "Open 12 Chests in Secret Caves", "types": ["secret_caves"] },
-    ],
+        [
+            { "name": "Enter 9 Warp Pots", "types": ["warp_pots", "dungeons"] },
+            { "name": "Collect 6 Dungeon Maps and Compasses", "types": ["maps_compasses", "dungeons"] },
+            { "name": "Open 3 Miniboss Chests", "types": ["miniboss", "dungeons"] }
+        ],
 
-    [
-        { "name": "Open 25 Dungeon Chests", "types": ["dungeon_chests", "dungeons"] },
-        { "name": "Open 4 Miniboss Chests", "types": ["miniboss", "dungeons"] },
-        { "name": "Break 4 Song Stones", "types": ["song_stones", "dungeons"] },
-    ],
+        [
+            { "name": "Defeat 3 Bosses", "types": ["boss", "dungeons"] },
+            { "name": "Open 20 Dungeon Chests", "types": ["dungeon_chests", "dungeons"] },
+            { "name": "Open 12 Chests in Secret Caves", "types": ["secret_caves"] }
+        ],
 
-    [
-        { "name": "Enter 12 Warp Pots", "types": ["warp_pots", "dungeons"] },
-        { "name": "Defeat 4 Bosses", "types": ["boss", "dungeons"] },
-        { "name": "Collect 8 Dungeon Maps and Compasses", "types": ["maps_compasses", "dungeons"] },
-    ],
-];
+        [
+            { "name": "Open 25 Dungeon Chests", "types": ["dungeon_chests", "dungeons"] },
+            { "name": "Open 4 Miniboss Chests", "types": ["miniboss", "dungeons"] },
+            { "name": "Break 4 Song Stones", "types": ["song_stones", "dungeons"] }
+        ],
+
+        [
+            { "name": "Enter 12 Warp Pots", "types": ["warp_pots", "dungeons"] },
+            { "name": "Defeat 4 Bosses", "types": ["boss", "dungeons"] },
+            { "name": "Collect 8 Dungeon Maps and Compasses", "types": ["maps_compasses", "dungeons"] }
+        ]
+    ]
+
+);
+
+bingoList.unshift([]);
 
 $(function () { srl.bingo(bingoList, 5); });
